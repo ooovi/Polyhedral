e7fdd144bc95430cf1a75c4ec4ebc7e7928fdf5e
ovi <ovi@posteo.de>
2025-10-15 16:03:09 +0200

    tidying up global variables

--- Polyhedral/Mathlib/Geometry/Convex/Cone/Pointed/Basic.lean
+++ Polyhedral/Mathlib/Geometry/Convex/Cone/Pointed/Basic.lean
@@ -13,10 +13,11 @@ namespace PointedCone
 open Module
 open Submodule hiding span dual restrict embed
 
+section Semiring
+
 variable {R E : Type*} [Semiring R] [PartialOrder R] [IsOrderedRing R] [AddCommMonoid E]
   [Module R E] {S : Set E}
 
-variable (R S) in
 -- /-- The span of a set `S` is the smallest pointed cone that contains `S`.
 
 -- Pointed cones being defined as submodules over nonnegative scalars, this is exactly the
@@ -28,22 +29,108 @@ variable (R S) in
 @[coe]
 abbrev ofSubmodule (S : Submodule R E) : PointedCone R E := S.restrictScalars _
 
-instance : Coe (Submodule R E) (PointedCone R E) := ⟨ofSubmodule _⟩
+instance : Coe (Submodule R E) (PointedCone R E) := ⟨ofSubmodule⟩
 
-lemma ofSubmodule.carrier_eq (S : Submodule R E) : (ofSubmodule _ S : Set E) = S :=
+lemma ofSubmodule.carrier_eq (S : Submodule R E) : (ofSubmodule S : Set E) = S :=
   coe_restrictScalars R S
 
-variable {R E : Type*} [Ring R] [LinearOrder R] [IsOrderedRing R] [AddCommGroup E]
+end Semiring
+
+section Semiring_AddCommGroup
+
+variable {R E : Type*} [Semiring R] [PartialOrder R] [IsOrderedRing R] [AddCommGroup E]
   [Module R E]
 
-lemma ofSubmodule_fg_of_fg {S : Submodule R E} (hS : S.FG) : (S : PointedCone R E).FG
-    := restrictedScalars_fg_of_fg _ hS
+-- TODO: implement sSup, sInf, sSup_map, sSupHomClass and sInfHomClass also for Submodule
 
-lemma fg_top [Module.Finite R E] : (⊤ : PointedCone R E).FG :=
-  ofSubmodule_fg_of_fg Module.Finite.fg_top
+@[simp]
+lemma sSup_coe (S : Set (Submodule R E)) : sSup S = sSup (ofSubmodule '' S) := by
+  ext x
+  -- we would like to use something like `Submodule.restrictScalars` for `sSup`.
+  -- we cannot use `map_sSup` because this needs `sSupHomClass`.
+  sorry
 
-lemma span_fg {C : PointedCone R E} (hC : C.FG) : (Submodule.span R (M := E) C).FG
-  := span_scalars_FG hC
+/-- The submodule span of a finitely generated pointed cone is finitely generated. -/
+lemma submodule_span_fg {C : PointedCone R E} (hC : C.FG) : (Submodule.span (M := E) R C).FG := by
+  obtain ⟨s, rfl⟩ := hC; use s; simp
+
+-- ### Neg
+
+-- TODO: should be built on `Submodule.pointwiseNeg` (I realized too late that this exists)
+instance : Neg (PointedCone R E) := ⟨map (f := -.id)⟩
+
+@[simp]
+lemma neg_neg (P : PointedCone R E) : - -P = P := by dsimp [Neg.neg]; ext x; simp
+
+instance : InvolutiveNeg (PointedCone R E) where
+  neg_neg := neg_neg
+
+lemma neg_coe {P : PointedCone R E} : (-P : PointedCone R E) = -(P : Set E) := by simp [Neg.neg]
+
+@[simp]
+lemma mem_neg {x : E} {P : PointedCone R E} : x ∈ -P ↔ -x ∈ P := by
+  rw [← SetLike.mem_coe, neg_coe]
+  exact Set.mem_neg
+
+-- @[simp]
+-- lemma neg_mem_neg {x : E} {P : PointedCone R E} : -x ∈ -P ↔ x ∈ P := by simp
+
+-- @[simp]
+-- lemma neg_inj {P Q : PointedCone R E} : -P = -Q ↔ P = Q := _root_.neg_inj -- has simp
+
+@[simp]
+lemma span_neg {s : Set E} : -(span R s) = span R (-s) := by
+  rw [← Set.image_neg_eq_neg];
+  exact Submodule.map_span _ _
+
+@[simp]
+lemma neg_inf {P Q : PointedCone R E} : -(P ⊓ Q) = -P ⊓ -Q := by ext x; simp
+
+@[simp]
+lemma neg_sup {P Q : PointedCone R E} : -(P ⊔ Q) = -P ⊔ -Q := by
+  sorry
+
+@[simp]
+lemma neg_top : -⊤ = (⊤ : PointedCone R E) := by ext x; simp
+
+@[simp]
+lemma neg_bot : -⊥ = (⊥ : PointedCone R E) := by ext x; simp
+
+-- NOTE: if this is implemented, it is more general than what mathlib already provides
+-- for converting submodules into pointedcones. Especially the proof that R≥0 is a FG
+-- submodule of R should be easiert with this.
+lemma span_union_neg_eq_span_submodule {s : Set E} :
+    span R (s ∪ -s) = Submodule.span R s := by
+  sorry
+
+lemma span_sup_neg_eq_span_submodule (C : PointedCone R E) :
+    C ⊔ -C = Submodule.span R (C : Set E) := by
+  sorry
+
+lemma span_eq_submodule_span_of_neg_self {s : Set E} (hs : s = -s) :
+    span R s = Submodule.span R s := by
+  sorry
+
+lemma neg_self_iff_eq_span_submodule (C : PointedCone R E) :
+    C = -C ↔ C = Submodule.span R (C : Set E) := by
+  sorry
+
+section Map
+
+variable {E' : Type*} [AddCommMonoid E'] [Module R E']
+
+lemma map_span (f : E →ₗ[R] E') (s : Set E) : map f (span R s) = span R (f '' s) := by
+  -- use `Submodule.map_span f s`
+  sorry
+
+end Map
+
+end Semiring_AddCommGroup
+
+
+section Ring
+
+variable {R E : Type*} [Ring R] [PartialOrder R] [IsOrderedRing R] [AddCommMonoid E] [Module R E]
 
 lemma coe_sup_submodule_span {C D : PointedCone R E} :
     Submodule.span R ((C : Set E) ⊔ (D : Set E)) = Submodule.span R (C ⊔ D : PointedCone R E) := by
@@ -132,96 +219,97 @@ lemma restrict_submodule_inf (S : Submodule R E) (C : PointedCone R E) :
 
 -- Submodule.submoduleOf_sup_of_le
 
-variable {R E : Type*} [Semiring R] [PartialOrder R] [IsOrderedRing R] [AddCommGroup E]
+
+end Ring
+
+
+section Ring_AddCommGroup
+
+variable {R E : Type*} [Ring R] [PartialOrder R] [IsOrderedRing R] [AddCommGroup E] [Module R E]
+
+-- ## Lineality
+
+/-- The lineality space of a cone. -/
+def lineal (C : PointedCone R E) : Submodule R E := sSup {S : Submodule R E | S ≤ C }
+
+lemma lineal_le (C : PointedCone R E) : C.lineal ≤ C := by simp [lineal] -- [sSup_coe]
+
+lemma le_lineal {C : PointedCone R E} {S : Submodule R E} (hS : S ≤ C) : S ≤ C.lineal := by
+  simp [lineal] -- [sSup_coe]
+  sorry
+
+@[simp]
+lemma lineal_submodule (S : Submodule R E) : (S : PointedCone R E).lineal = S := by
+  sorry
+
+@[simp] lemma lineal_top : (⊤ : PointedCone R E).lineal = ⊤ := lineal_submodule ⊤
+@[simp] lemma lineal_bot : (⊥ : PointedCone R E).lineal = ⊥ := lineal_submodule ⊥
+
+-- @[simp] -- no simp because right side has twice as many `x`?
+lemma lineal_mem {x : E} {C : PointedCone R E} : x ∈ C.lineal ↔ x ∈ C ∧ -x ∈ C := by
+  constructor
+  · intro h
+    have h' := neg_mem_iff.mpr h
+    exact ⟨lineal_le C h, lineal_le C h'⟩
+  · intro ⟨h, h'⟩
+    let S := Submodule.span R {x, -x}
+    have hSC : S ≤ C := by sorry
+    have hSC := le_lineal hSC
+    have hxS : x ∈ S := Submodule.mem_span_of_mem (by simp)
+    exact hSC hxS -- maybe we could use the lemma that s ∪ -s spans a linear space (see above)
+
+lemma lineal_eq_inf_neg (C : PointedCone R E) : C.lineal = C ⊓ -C := by
+  ext x; simp [lineal_mem]
+
+@[simp]
+lemma lineal_inf (C D : PointedCone R E) : (C ⊓ D).lineal = C.lineal ⊓ D.lineal := by
+  ext x; simp [lineal_mem]; aesop
+
+
+lemma span_inter_lineal_eq_lineal {C : PointedCone R E} {s : Set E} (h : span R s = C) :
+    span R (s ∩ C.lineal) = C.lineal := by
+  rw [← antisymm_iff (r := LE.le)]
+  constructor
+  · rw [← Submodule.span_eq (C.lineal : PointedCone R E)]
+    exact Submodule.span_mono (by simp)
+  · rw [← SetLike.coe_subset_coe]
+    rw [Set.subset_def]
+    intro x hx
+    let S:= s ∩ C.lineal
+    let S' := s \ C.lineal
+    have hS : S ∪ S' = s := by simp [S, S']
+    have hS' : S ∩ S' = ∅ := by aesop
+
+    --have hs : s = (s ∩ C.lineal) ∪ ()
+    -- rw [Submodule.mem_span_finite_of_mem_span] at h
+    sorry
+
+end Ring_AddCommGroup
+
+
+section Ring_LinearOrder
+
+-- we have LinearOrder because then Module.Finite { c // 0 ≤ c } R
+variable {R E : Type*} [Ring R] [LinearOrder R] [IsOrderedRing R] [AddCommMonoid E]
   [Module R E]
 
--- TODO: implement sSup, sInf, sSup_map, sSupHomClass and sInfHomClass also for Submodule
+lemma ofSubmodule_fg_of_fg {S : Submodule R E} (hS : S.FG) : (S : PointedCone R E).FG
+    := restrictedScalars_fg_of_fg _ hS
 
-@[simp]
-lemma sSup_coe (S : Set (Submodule R E)) : sSup S = sSup (ofSubmodule _ '' S) := by
-  ext x
-  -- we would like to use something like `Submodule.restrictScalars` for `sSup`.
-  -- we cannot use `map_sSup` because this needs `sSupHomClass`.
-  sorry
+lemma fg_top [Module.Finite R E] : (⊤ : PointedCone R E).FG :=
+  ofSubmodule_fg_of_fg Module.Finite.fg_top
 
-/-- The submodule span of a finitely generated pointed cone is finitely generated. -/
-lemma submodule_span_fg {C : PointedCone R E} (hC : C.FG) : (Submodule.span (M := E) R C).FG := by
-  obtain ⟨s, rfl⟩ := hC; use s; simp
+lemma span_fg {C : PointedCone R E} (hC : C.FG) : (Submodule.span R (M := E) C).FG
+  := span_scalars_FG hC
 
--- ### Neg
+end Ring_LinearOrder
 
--- TODO: should be built on `Submodule.pointwiseNeg` (I realized too late that this exists)
-instance : Neg (PointedCone R E) := ⟨map (f := -.id)⟩
-
-@[simp]
-lemma neg_neg (P : PointedCone R E) : - -P = P := by dsimp [Neg.neg]; ext x; simp
-
-instance : InvolutiveNeg (PointedCone R E) where
-  neg_neg := neg_neg
-
-lemma neg_coe {P : PointedCone R E} : (-P : PointedCone R E) = -(P : Set E) := by simp [Neg.neg]
-
-@[simp]
-lemma mem_neg {x : E} {P : PointedCone R E} : x ∈ -P ↔ -x ∈ P := by
-  rw [← SetLike.mem_coe, neg_coe]
-  exact Set.mem_neg
-
--- @[simp]
--- lemma neg_mem_neg {x : E} {P : PointedCone R E} : -x ∈ -P ↔ x ∈ P := by simp
-
--- @[simp]
--- lemma neg_inj {P Q : PointedCone R E} : -P = -Q ↔ P = Q := _root_.neg_inj -- has simp
-
-@[simp]
-lemma span_neg {s : Set E} : -(span R s) = span R (-s) := by
-  rw [← Set.image_neg_eq_neg];
-  exact Submodule.map_span _ _
-
-@[simp]
-lemma neg_inf {P Q : PointedCone R E} : -(P ⊓ Q) = -P ⊓ -Q := by ext x; simp
-
-@[simp]
-lemma neg_sup {P Q : PointedCone R E} : -(P ⊔ Q) = -P ⊔ -Q := by
-  sorry
-
-@[simp]
-lemma neg_top : -⊤ = (⊤ : PointedCone R E) := by ext x; simp
-
-@[simp]
-lemma neg_bot : -⊥ = (⊥ : PointedCone R E) := by ext x; simp
-
--- NOTE: if this is implemented, it is more general than what mathlib already provides
--- for converting submodules into pointedcones. Especially the proof that R≥0 is a FG
--- submodule of R should be easiert with this.
-lemma span_union_neg_eq_span_submodule {s : Set E} :
-    span R (s ∪ -s) = Submodule.span R s := by
-  sorry
-
-lemma span_sup_neg_eq_span_submodule (C : PointedCone R E) :
-    C ⊔ -C = Submodule.span R (C : Set E) := by
-  sorry
-
-lemma span_eq_submodule_span_of_neg_self {s : Set E} (hs : s = -s) :
-    span R s = Submodule.span R s := by
-  sorry
-
-lemma neg_self_iff_eq_span_submodule (C : PointedCone R E) :
-    C = -C ↔ C = Submodule.span R (C : Set E) := by
-  sorry
-
-section Map
-
-variable {E' : Type*} [AddCommMonoid E'] [Module R E']
-
-lemma map_span (f : E →ₗ[R] E') (s : Set E) : map f (span R s) = span R (f '' s) := by
-  -- use `Submodule.map_span f s`
-  sorry
-
-end Map
 
 /- Duality -/
 
-variable {R F : Type*} [CommRing R] [PartialOrder R] [IsOrderedRing R]
+section CommRing
+
+variable {R E F : Type*} [CommRing R] [PartialOrder R] [IsOrderedRing R] [AddCommGroup E]
   [Module R E] [AddCommGroup F] [Module R F] {p : E →ₗ[R] F →ₗ[R] R}
 
 lemma dual_bilin_dual_id (s : Set E) : dual p s = dual .id (p '' s) := by ext x; simp
@@ -261,49 +349,12 @@ example {C C' : PointedCone R E} : -- (hC : C.FG) (hC' : C'.FG) :
   sorry
 
 
--- ## Lineality
-
-/-- The lineality space of a cone. -/
-def lineal (C : PointedCone R E) := sSup {S : Submodule R E | S ≤ C }
-
-lemma lineal_le (C : PointedCone R E) : C.lineal ≤ C := by simp [lineal] -- [sSup_coe]
-
-lemma le_lineal {C : PointedCone R E} {S : Submodule R E} (hS : S ≤ C) : S ≤ C.lineal := by
-  simp [lineal] -- [sSup_coe]
-  sorry
-
-@[simp]
-lemma lineal_submodule (S : Submodule R E) : (S : PointedCone R E).lineal = S := by
-  sorry
-
-@[simp] lemma lineal_top : (⊤ : PointedCone R E).lineal = ⊤ := lineal_submodule ⊤
-@[simp] lemma lineal_bot : (⊥ : PointedCone R E).lineal = ⊥ := lineal_submodule ⊥
-
--- @[simp] -- no simp because right side has twice as many `x`?
-lemma lineal_mem {x : E} {C : PointedCone R E} : x ∈ C.lineal ↔ x ∈ C ∧ -x ∈ C := by
-  constructor
-  · intro h
-    have h' := neg_mem_iff.mpr h
-    exact ⟨lineal_le C h, lineal_le C h'⟩
-  · intro ⟨h, h'⟩
-    let S := Submodule.span R {x, -x}
-    have hSC : S ≤ C := by sorry
-    have hSC := le_lineal hSC
-    have hxS : x ∈ S := Submodule.mem_span_of_mem (by simp)
-    exact hSC hxS -- maybe we could use the lemma that s ∪ -s spans a linear space (see above)
-
-lemma lineal_eq_inf_neg (C : PointedCone R E) : C.lineal = C ⊓ -C := by
-  ext x; simp [lineal_mem]
-
-@[simp]
-lemma lineal_inf (C D : PointedCone R E) : (C ⊓ D).lineal = C.lineal ⊓ D.lineal := by
-  ext x; simp [lineal_mem]; aesop
 
 -- the other direction does not hold in general (consider a cone with lineality space and now
 --  delete every points from that lineality space except for the origin).
 --  It holds for FG (and CoFG?)
 -- Q: do I need p.IsPerfPair?
-lemma span_dual_eq_sual_lineal [p.IsPerfPair] (C : PointedCone R E) :
+lemma span_dual_eq_dual_lineal [p.IsPerfPair] (C : PointedCone R E) :
     Submodule.span R (dual p C) ≤ .dual p C.lineal := by
   -- simp [lineal_mem]
   -- C.lin ≤ C
@@ -311,30 +362,18 @@ lemma span_dual_eq_sual_lineal [p.IsPerfPair] (C : PointedCone R E) :
   -- hence span dual C ≤ span dual C.lin = dual C.lin
   sorry
 
+end CommRing
+
+section Ring_AddCommGroup
+
+variable {R E : Type*} [Ring R] [PartialOrder R] [IsOrderedRing R] [AddCommGroup E] [Module R E]
+
 -- ## IsPointy
 
 -- TODO: use `ConvexCone.Salient`
 
 /-- A pointy cone has trivial lineality space. -/
 def IsPointy (C : PointedCone R E) := C.lineal = ⊥
-
-lemma span_inter_lineal_eq_lineal {C : PointedCone R E} {s : Set E} (h : span R s = C) :
-    span R (s ∩ C.lineal) = C.lineal := by
-  rw [← antisymm_iff (r := LE.le)]
-  constructor
-  · rw [← Submodule.span_eq (C.lineal : PointedCone R E)]
-    exact Submodule.span_mono (by simp)
-  · rw [← SetLike.coe_subset_coe]
-    rw [Set.subset_def]
-    intro x hx
-    let S:= s ∩ C.lineal
-    let S' := s \ C.lineal
-    have hS : S ∪ S' = s := by simp [S, S']
-    have hS' : S ∩ S' = ∅ := by aesop
-
-    --have hs : s = (s ∩ C.lineal) ∪ ()
-    -- rw [Submodule.mem_span_finite_of_mem_span] at h
-    sorry
 
 lemma span_diff_lineal_pointy {C : PointedCone R E} {s : Set E} (h : span R s = C) :
     (span R (s \ C.lineal)).IsPointy := by
@@ -379,5 +418,7 @@ lemma sup_inf_assoc_of_le_submodule {C : PointedCone R E} (D : PointedCone R E)
       rw [Submodule.restrictScalars_mem, ← Submodule.neg_mem_iff] at hCE
       exact Submodule.add_mem E hCE hx
     ⟩, hyzx⟩
+
+end Ring_AddCommGroup
 
 end PointedCone
