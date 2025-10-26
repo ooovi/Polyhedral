bc39bc079c9d1311d98c4a19ee9a69eaeb4d4172
Martin Winter <martin.winter.math@gmail.com>
2025-10-26 21:43:15 +0100

    Cleaning up `Submodule.Basic`

--- Polyhedral/Mathlib/Algebra/Module/Submodule/Basic.lean
+++ Polyhedral/Mathlib/Algebra/Module/Submodule/Basic.lean
@@ -8,9 +8,14 @@ import Mathlib.RingTheory.Noetherian.Defs
 
 namespace Submodule
 
+open Function
+
 section Semiring
 
 variable {M S R : Type*} [Semiring R] [AddCommMonoid M] [Module R M]
+
+
+-- ## SPAN
 
 -- Is this the better name?
 protected alias span_gi := Submodule.gi
@@ -49,16 +54,35 @@ lemma span_biInter (s : Set (Submodule R M)) :
 
 alias span_iInter := span_biInter
 
+
+-- ## RESTRICT / EMBED
+
 /- I suggest the alternative naming `restrict` for `submoduleOf` for the following reason:
   we want to have the same functionality on `PointedCone`, but there the name `submoduleOf`
-  makes no sense. Using a different name would be inconsistent.
+  makes no sense. Using the same name is preferred for consistency.
   Not also that this cannot be an alias of `submoduleOf` because the argument are in the
   opposite order. This is in order to be consistent with `embed` for which the other order
-  is not possible.
+  is not possible. This also allows us to view `restrict S` as the restriction operator.
 -/
 
-/-- The restriction of `S ⊓ T` considered as a submodule of `S`. -/
+/-- The restriction of `S ⊓ T` reinterpreted as a submodule of `S`. -/
 abbrev restrict (S T : Submodule R M) : Submodule R S := T.submoduleOf S -- T.comap S.subtype
+
+-- Q: Shoud have `restrict` as an order hom? If yes, use the code below.
+def restrict' (S : Submodule R M) : Submodule R M →o Submodule R S where
+  toFun := (submoduleOf · S)
+  monotone' := fun _ _ h _ => (h ·)
+
+@[simp] lemma restrict_top (S : Submodule R M) : restrict S ⊤ = ⊤ := by
+  simp only [submoduleOf, comap_top]
+@[simp] lemma restrict_bot (S : Submodule R M) : restrict S ⊥ = ⊥ := by
+  simp only [submoduleOf, comap_bot, ker_subtype]
+
+lemma mem_restrict {S : Submodule R M} {T : Submodule R M} {x : S} (h : x ∈ restrict S T) :
+    (x : M) ∈ T := by simpa using h
+
+lemma mem_restrict_iff {S : Submodule R M} {T : Submodule R M} {x : S} :
+    x ∈ restrict S T ↔ (x : M) ∈ T := by simp [submoduleOf]
 
 @[simp] lemma restrict_self (S : Submodule R M) : restrict S S = ⊤ := submoduleOf_self S
 
@@ -69,28 +93,89 @@ lemma restrict_inf (S : Submodule R M) {T₁ T₂ : Submodule R M} :
     restrict S (T₁ ⊓ T₂) = (restrict S T₁) ⊓ (restrict S T₂)
   := by ext x; simp [restrict, submoduleOf]
 
-/-- A submodule `T` of a submodule `S` of `M` intepreted as a submodule of `M`. -/
+/-- A submodule `T` of a submodule `S` of `M` reintepreted as a submodule of `M`. -/
 abbrev embed {S : Submodule R M} (T : Submodule R S) : Submodule R M := T.map S.subtype
+
+-- Q: Shoud have `embed` as an order embedding? If yes, use the code below.
+def embed' {S : Submodule R M} : Submodule R S ↪o Submodule R M where
+  toFun := map S.subtype
+  inj' := map_injective_of_injective S.subtype_injective
+  map_rel_iff' := ⟨fun h x => by simpa using @h x, Submodule.map_mono⟩
+
+lemma mem_embed_iff {S : Submodule R M} {T : Submodule R S} (x : S) :
+    (x : M) ∈ embed T ↔ x ∈ T := by simp
+
+lemma mem_of_mem_embed {S : Submodule R M} {T : Submodule R S} {x : M} (hx : x ∈ embed T) :
+    x ∈ S := by
+  simp only [mem_map, subtype_apply, Subtype.exists, exists_and_right, exists_eq_right] at hx
+  obtain ⟨hxS, _⟩ := hx
+  exact hxS
+
+abbrev of_mem_embed {S : Submodule R M} {T : Submodule R S} {x : M} (hx : x ∈ embed T) : S :=
+  ⟨x, mem_of_mem_embed hx⟩
+
+lemma of_mem_embed_mem {S : Submodule R M} {T : Submodule R S} {x : M} (hx : x ∈ embed T) :
+    of_mem_embed hx ∈ T := by
+  simp only [mem_map, subtype_apply, Subtype.exists, exists_and_right, exists_eq_right] at hx
+  obtain ⟨_, h⟩ := hx
+  exact h
 
 @[simp] lemma embed_restrict (S T : Submodule R M) : embed (restrict S T) = S ⊓ T
   := map_comap_subtype _ _
 
--- @[simp] lemma restrict_embed {S : Submodule R M} (T : Submodule R S) : restrict S (embed T) = T
---   := sorry
+@[simp] lemma restrict_embed {S : Submodule R M} (T : Submodule R S) : restrict S (embed T) = T
+    := by ext x; simp [submoduleOf]
 
-lemma embed_inj {S : Submodule R M} {T₁ T₂ : Submodule R S} (hT : embed T₁ = embed T₂) :
-    T₁ = T₂ := by sorry
+lemma embed_injective {S : Submodule R M} : Injective (embed (S := S))
+  := map_injective_of_injective S.subtype_injective
 
--- lemma embed_mono {S : Submodule R M} {T₁ T₂ : Submodule R S} (hT : embed T₁ ≤ embed T₂) :
---     T₁ ≤ T₂ := sorry
+@[simp] lemma embed_inj {S : Submodule R M} {T₁ T₂ : Submodule R S} :
+    embed T₁ = embed T₂ ↔ T₁ = T₂ := Injective.eq_iff embed_injective
 
-def embed_orderEmbed (S : Submodule R M) : OrderEmbedding (Submodule R S) (Submodule R M) where
+lemma embed_mono {S : Submodule R M} {T₁ T₂ : Submodule R S} (hT : T₁ ≤ T₂) :
+    embed T₁ ≤ embed T₂ := Submodule.map_mono hT
+
+lemma embed_mono_rev {S : Submodule R M} {T₁ T₂ : Submodule R S} (hT : embed T₁ ≤ embed T₂) :
+    T₁ ≤ T₂ := (by simpa using @hT ·)
+
+@[simp] lemma embed_mono_iff {S : Submodule R M} {T₁ T₂ : Submodule R S} :
+    embed T₁ ≤ embed T₂ ↔ T₁ ≤ T₂ where
+  mp := embed_mono_rev
+  mpr := embed_mono
+
+@[simp] lemma embed_top (S : Submodule R M) : embed (⊤ : Submodule R S) = S := by simp
+@[simp] lemma embed_bot (S : Submodule R M) : embed (⊥ : Submodule R S) = ⊥ := map_bot _
+
+lemma embed_le {S : Submodule R M} (T : Submodule R S) : embed T ≤ S := by
+  simpa using embed_mono le_top
+
+def embed_orderEmbed (S : Submodule R M) : Submodule R S ↪o Submodule R M where
   toFun := embed
-  inj' := @embed_inj _ _ _ _ _ S
-  map_rel_iff' := by
-    intro T U
-    simp
+  inj' := embed_injective
+  map_rel_iff' := embed_mono_iff
+
+def embed_CompleteLatticeHom (S : Submodule R M) :
+    CompleteLatticeHom (Submodule R S) (Submodule R M) where
+  toFun := embed
+  map_sInf' := by
+    intro s
+    unfold embed
     sorry
+  map_sSup' := by
+    intro s
+    unfold embed
+    sorry
+
+def embed_gi (S : Submodule R M) : GaloisCoinsertion (embed) (restrict S) where
+  choice := fun T _ => restrict S T
+  gc T U := by
+    simp [← embed_mono_iff, embed_restrict, le_inf_iff, iff_and_self]
+    exact fun _ => embed_le T
+  u_l_le T := le_of_eq (restrict_embed T)
+  choice_eq := by simp
+
+
+-- ## RESTRICT SCALARS
 
 section RestrictedScalar
 
@@ -115,28 +200,6 @@ open Function
   (restrictScalarsLatticeHom S R M).map_sInf' s
 
 end RestrictedScalar
-
-section Ring
-
-variable {M R : Type*} [Ring R] [AddCommGroup M] [Module R M]
-
--- Q: Does this really need Ring?
-@[simp] lemma restrict_embed {S : Submodule R M} (T : Submodule R S) : restrict S (embed T) = T
-  := by simp [submoduleOf, comap_map_eq]
-
-def embed_gi (S : Submodule R M) : GaloisCoinsertion (embed) (restrict S) where
-  choice := fun T _ => restrict S T
-  gc T U := by
-    constructor
-    · intro h
-      sorry
-    · intro h
-      sorry
-  u_l_le T := le_of_eq (restrict_embed T)
-  choice_eq := by simp
-
-
-end Ring
 
 -- section Field
 
