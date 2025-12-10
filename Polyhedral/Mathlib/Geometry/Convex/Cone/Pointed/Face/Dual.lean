4a93369e0aca8854fb19863e1517f4ac02c57dbd
Martin Winter <martin.winter.math@gmail.com>
2025-12-10 11:47:31 +0100

    New Dual file

---
+++ Polyhedral/Mathlib/Geometry/Convex/Cone/Pointed/Face/Dual.lean
@@ -0,0 +1,22 @@
+/-
+Copyright (c) 2025 Olivia Röhrig. All rights reserved.
+Released under Apache 2.0 license as described in the file LICENSE.
+Authors: Olivia Röhrig
+-- -/
+import Mathlib.Analysis.Convex.Extreme
+import Mathlib.LinearAlgebra.Quotient.Basic
+import Mathlib.Order.Atoms
+import Mathlib.Order.Atoms.Finite
+
+import Polyhedral.Mathlib.Geometry.Convex.Cone.Pointed.Dual
+
+
+/-!
+# Faces of pointed cones
+-/
+
+namespace PointedCone
+
+variable {R M N : Type*}
+
+end PointedCone
