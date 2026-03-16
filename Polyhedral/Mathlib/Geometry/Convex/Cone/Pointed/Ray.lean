17d3b84d2c14338b3853f79b7c02251fe139c824
ovi <ovi@posteo.de>
2026-03-16 16:09:33 +0100

    formatting

--- Polyhedral/Mathlib/Geometry/Convex/Cone/Pointed/Ray.lean
+++ Polyhedral/Mathlib/Geometry/Convex/Cone/Pointed/Ray.lean
@@ -2,6 +2,27 @@ import Polyhedral.Mathlib.Geometry.Convex.Cone.Pointed.Rank
 
 namespace PointedCone
 
+section Ring
+
+variable {R M : Type*}
+variable [Ring R] [StrongRankCondition R] [PartialOrder R] [IsOrderedRing R] [IsDomain R]
+variable [AddCommGroup M] [Module R M] [Module.IsTorsionFree R M]
+
+lemma rank_one_of_ray {x : M} (hx : x ≠ 0) : (span R {x}).rank = 1 := by
+  have hlin : LinearIndependent R (fun _ : Unit => x) :=
+    LinearIndependent.of_subsingleton () hx
+  have hr := rank_span (R := R) (M := M) hlin
+  have hspan : Submodule.span R (Set.range (fun _ : Unit => x)) = (span R {x}).linSpan := by
+    simp [PointedCone.linSpan, Set.range_const]
+  rw [hspan] at hr
+  rw [PointedCone.rank]
+  simpa using hr
+
+lemma finrank_one_of_ray {x : M} (hx : x ≠ 0) : (span R {x}).finrank = 1 := by
+  simpa [Module.finrank, Cardinal.toNat_eq_one] using rank_one_of_ray hx
+
+end Ring
+
 section DivisionRing
 
 variable {R : Type*} [DivisionRing R] [LinearOrder R] [IsOrderedRing R]
@@ -41,27 +62,5 @@ lemma ray_of_rank_one (hS : C.Salient) (h : C.rank = 1) : ∃ x : M, C = span R 
   simp [h] at this
 
 end DivisionRing
-
-section Ring
-
-variable {R : Type*} [Ring R] [StrongRankCondition R] [PartialOrder R] [IsOrderedRing R] [IsDomain R]
-variable {M : Type*} [AddCommGroup M] [Module R M] [Module.IsTorsionFree R M]
-
-lemma rank_one_of_ray {x : M} (hx : x ≠ 0) :
-    (span R {x}).rank = 1 := by
-  have hlin : LinearIndependent R (fun _ : Unit => x) :=
-    LinearIndependent.of_subsingleton () hx
-  have hr := rank_span (R := R) (M := M) hlin
-  have hspan : Submodule.span R (Set.range (fun _ : Unit => x)) = (span R {x}).linSpan := by
-    simp [PointedCone.linSpan, Set.range_const]
-  rw [hspan] at hr
-  rw [PointedCone.rank]
-  simpa using hr
-
-lemma finrank_one_of_ray {x : M} (hx : x ≠ 0) :
-    (span R {x}).finrank = 1 := by
-  simpa [Module.finrank, Cardinal.toNat_eq_one] using rank_one_of_ray hx
-
-end Ring
 
 end PointedCone
