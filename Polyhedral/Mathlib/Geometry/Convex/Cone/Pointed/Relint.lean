c40f00eba847fe2bfc8f044bb58313adbdc49132
Martin Winter <martin.winter.math@gmail.com>
2026-03-22 02:50:44 +0100

    Add relint description in comment

--- Polyhedral/Mathlib/Geometry/Convex/Cone/Pointed/Relint.lean
+++ Polyhedral/Mathlib/Geometry/Convex/Cone/Pointed/Relint.lean
@@ -21,18 +21,44 @@ open PointedCone
 
 -- ## RELINT
 
+/-
+The relative interior (relint for short) is a topologyical notion, and hence might depend on the
+exact topology chosen on the ambient space. In finite dimensions, the topology is essentially
+unique, but in infinite dimensions it is possible that a cone has a non-empty or empty relint
+depending on the topology.
+
+Without a topology one can consider algebraic notions of relative interior.
+
+The `core` is one possible notion with the following equivalent definitions: a point x ∈ C lies in
+the core iff one of the following equivalent conditions holds:
+  * x lies in no proper face
+  * hull R (C ∪ (-x)) = span R C
+  * ∀ t : M, ∃ ε > 0, x + ε • t ∈ C
+  * ∀ φ : Dual R M, φ x = 0 → φ ∈ (dual (Dual.eval R M) C).lineal
+
+The `weak relint` is another notion that is not always equivalent to the core. It is the relative
+interior w.r.t. the topology obtain from the double-dual closure operation (or, weak topology).
+
+In finite dimensions all these notions are the same, while in infinite dimensions this is not
+true anymore. At least we always have `weak relint ≤ core`.
+
+The core has moreover the property that the cone is the disjoint union of the cores of the faces.
+This is not true for the weak relint. One still has disjointness of the relints, but not that they
+cover all of the cone.
+-/
+
+/- TODO:
+  * the relints of the faces of a cone partition the cone.
+-/
+
 namespace PointedCone
 
 variable {R : Type*} [Field R] [LinearOrder R] [IsOrderedRing R]
 variable {M : Type*} [AddCommGroup M] [Module R M]
 variable {N : Type*} [AddCommGroup N] [Module R N]
 variable {p : M →ₗ[R] N →ₗ[R] R}
-variable {C F F₁ F₂ : PointedCone R M}
+variable {C D F F₁ F₂ : PointedCone R M}
 variable {x : M}
-
-/- A non-topological variant of the relative interior.
-  There are many ways to define it, and hopefully all of them are equivalent.
--/
 
 def relint (C : PointedCone R M) : ConvexCone R M where
   carrier := {x ∈ C | span R (insert (-x) C) = ⊤}
@@ -42,10 +68,10 @@ def relint (C : PointedCone R M) : ConvexCone R M where
 lemma mem_relint_iff_mem_span_neg_eq_top :
     x ∈ C.relint ↔ x ∈ C ∧ span R (insert (-x) C) = ⊤ := by simp [relint]
 
-lemma relint_le : C.relint ≤ C := fun _ hx => (mem_relint_iff_mem_span_neg_eq_top.mp hx).1
+lemma relint_le : C.relint ≤ C := fun _ hx => hx.1
 
 lemma mem_relint_iff_mem_forall_isFaceOf_eq_top_of_mem :
-    x ∈ C.relint ↔ x ∈ C ∧ ∀ F : PointedCone R M, F.IsFaceOf C → (x ∈ F → F = ⊤) := by
+    x ∈ C.relint ↔ x ∈ C ∧ ∀ F : PointedCone R M, F.IsFaceOf C → (x ∈ F → F = C) := by
   sorry
 
 lemma mem_relint_iff_mem_forall_face_eq_top_of_mem :
@@ -56,8 +82,15 @@ lemma mem_relint_iff_forall_dual_zero_le_mem_lineal_of_eq_zero :
     x ∈ C.relint ↔ x ∈ C ∧ ∀ y ∈ dual p C, 0 ≤ p x y ∧ (p x y = 0 → y ∈ (dual p C).lineal) := by
   sorry
 
-lemma finset_sum_mem_relint_span {s : Finset M} (hs : (s : Set M) ⊆ C)
-    (hC : C ≤ Submodule.span (M := M) R s) : ∑ x ∈ s, x ∈ relint (span R (s : Set M)) := by
+lemma mem_relint_iff_forall_exists_gt_zero_forall_le_add_smul_mem :
+    x ∈ C.relint ↔ x ∈ C ∧ ∀ t : M, ∃ ε > 0, x + ε • t ∈ C := by
+  sorry
+
+lemma finset_sum_mem_relint_of_subset_of_le_span {s : Finset M} (hs : (s : Set M) ⊆ C)
+    (hC : C ≤ Submodule.span (M := M) R s) : ∑ x ∈ s, x ∈ relint C := by
+  sorry
+
+lemma finset_sum_mem_relint_span {s : Finset M} : ∑ x ∈ s, x ∈ relint (span R (s : Set M)) := by
   sorry
 
 lemma relint_nonempty (h : C.FinSalRank) : Nonempty C.relint := sorry
@@ -67,8 +100,10 @@ lemma relint_nonempty' (h : C.FinRank) : Nonempty C.relint := by
   /-
     * choose a basis in C
     * since C.FinRank, this basis is finite
-    * use `finset_sum_mem_relint_span`
+    * use `finset_sum_mem_relint_of_subset_of_le_span`
   -/
   sorry
+
+lemma ofSubmodule_relint (S : Submodule R M) : (S : PointedCone R M).relint = S := sorry
 
 end PointedCone
