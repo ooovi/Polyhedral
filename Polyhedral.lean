fdebd9f113dbd3eb751494b09983c01426f213c2
Martin Winter <martin.winter.math@gmail.com>
2026-07-05 00:24:20 +0200

    mk_all

--- Polyhedral.lean
+++ Polyhedral.lean
@@ -63,6 +63,7 @@ import Polyhedral.Mathlib.LinearAlgebra.AffineSpace.Homogenization.Set
 import Polyhedral.Mathlib.LinearAlgebra.AffineSpace.Lattice
 import Polyhedral.Mathlib.LinearAlgebra.BilinearMap
 import Polyhedral.Mathlib.LinearAlgebra.Dual.Basis
+import Polyhedral.Mathlib.Logic.Function.Const
 import Polyhedral.Mathlib.RingTheory.Finiteness.Cofinite
 import Polyhedral.Mathlib.RingTheory.Finiteness.Corank
 import Polyhedral.Polyhedral.Basic
