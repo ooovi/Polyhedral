4365c42bdb770b9f068c8fbe9c2e095ac51cb514
Martin Winter <martin.winter.math@gmail.com>
2026-08-13 16:46:32 +0200

    Cleanup Corank.lean

--- Polyhedral/Mathlib/RingTheory/Finiteness/Corank.lean
+++ Polyhedral/Mathlib/RingTheory/Finiteness/Corank.lean
@@ -3,50 +3,130 @@ Copyright (c) 2026 Martin Winter. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Martin Winter
 -/
+module
 
-import Mathlib.LinearAlgebra.Dimension.Basic
-import Mathlib.LinearAlgebra.Dimension.Free
-import Mathlib.LinearAlgebra.Projection
-import Mathlib.LinearAlgebra.Dimension.RankNullity
+public import Mathlib.LinearAlgebra.Dimension.RankNullity
 
-import Polyhedral.Mathlib.Algebra.Module.Submodule.Basic
+/-!
+# Corank of a submodule
 
-/-! This file defines the corank of a submodule and proves basic properties. -/
+For a submodule `p : Submodule R M`, its `corank` is the rank of the quotient
+`M ⧸ p`.
+-/
 
-open Module Cardinal
+/-
+# NOTE:
+This file defines corank only over rings. One can generalize to semiring using the
+following alternive definitions, which also naturally extend to spanCorank:
+
+```
+noncomputable def corank (S : Submodule R M) : Cardinal :=
+  ⨆ T : { T : Submodule R M // Disjoint T S },
+    Module.rank R T.1
+
+noncomputable def spanCorank (S : Submodule R M) : Cardinal :=
+  ⨅ T : { T : Submodule R M // Codisjoint S T },
+    T.1.spanRank
+```
+
+Over rings we obtain the following theorems:
+
+````
+theorem corank_eq_rank_quotient :
+    S.corank = Module.rank R (M ⧸ S)
+
+theorem spanCorank_eq_spanRank_quotient :
+    S.spanCorank = (⊤ : Submodule R (M ⧸ S)).spanRank
+```
+-/
+
+public section
+
+universe u v w
+
+variable {R M N : Type*}
 
 namespace Submodule
 
-section Semiring
+variable [Ring R] [AddCommGroup M] [Module R M]
 
-variable {R : Type*} [Semiring R] {M : Type*} [AddCommMonoid M] [Module R M]
+/-- The corank of a submodule `p` is the rank of the quotient `M ⧸ p`. -/
+noncomputable def corank (p : Submodule R M) : Cardinal :=
+  Module.rank R (M ⧸ p)
 
-noncomputable def spanCorank (S : Submodule R M) :=
-  ⨅ ι : { s : Submodule R M // span R s ⊔ S = ⊤ }, (Module.rank R ι.1)
+theorem corank_def (p : Submodule R M) :
+    p.corank = Module.rank R (M ⧸ p) :=
+  by rfl
 
-noncomputable def corank' (S : Submodule R M) :=
-  ⨆ ι : { s : Submodule R M // span R s ⊓ S = ⊥ }, (Module.rank R ι.1)
+/-- The corank of a submodule is at most the rank of the ambient module. -/
+theorem corank_le (p : Submodule R M) :
+    p.corank ≤ Module.rank R M := by
+  simpa [corank] using rank_quotient_le p
 
-end Semiring
+theorem corank_anti {p q : Submodule R M} (h : p ≤ q) :
+    q.corank ≤ p.corank := by
+  simpa [corank] using (factor h).rank_le_of_surjective (factor_surjective h)
 
-section Ring
+theorem corank_antitone :
+    Antitone (corank : Submodule R M → Cardinal) :=
+  fun _ _ => corank_anti
 
-variable {R : Type*} [Ring R] {M : Type*} [AddCommGroup M] [Module R M]
+@[simp]
+theorem corank_bot : (⊥ : Submodule R M).corank = Module.rank R M := by
+  simpa [corank] using ((⊥ : Submodule R M).quotEquivOfEqBot rfl).rank_eq
 
-noncomputable abbrev corank (S : Submodule R M) :
-    Cardinal := Module.rank R (M ⧸ S)
+@[simp]
+theorem corank_top [Nontrivial R] : (⊤ : Submodule R M).corank = 0 := by
+  rw [corank, rank_eq_zero_iff]
+  exact fun _ => ⟨1, one_ne_zero, Subsingleton.elim _ _⟩
 
-lemma Submodule.corank_eq_inf_linearIndep (S : Submodule R M) :
-    corank S = ⨅ ι : { s : Set M // S ⊔ span R s = ⊤}, (#ι.1) := by
-  sorry
+end Submodule
 
-lemma rank_add_corank (S : Submodule R M) : Module.rank R S + corank S = Module.rank R M :=
-  sorry
+namespace LinearEquiv
 
-lemma rank_eq_corank_of_isCompl {S T : Submodule R M} (h : IsCompl S T) :
-    Module.rank R S = corank T :=
-  sorry
+variable [Ring R]
 
-end Ring
+variable {M : Type v} {N : Type w}
+variable [AddCommGroup M] [Module R M]
+variable [AddCommGroup N] [Module R N]
+
+/-- Two linearly equivalent modules have the same corank, a version with different universes. -/
+theorem lift_corank_map_eq (e : M ≃ₗ[R] N) (p : Submodule R M) :
+    Cardinal.lift.{v, w} (p.map (e : M →ₗ[R] N)).corank =
+      Cardinal.lift.{w, v} p.corank := by
+  exact (Submodule.Quotient.equiv p (p.map (e : M →ₗ[R] N)) e rfl).lift_rank_eq.symm
+
+variable {M N : Type v}
+variable [AddCommGroup M] [Module R M]
+variable [AddCommGroup N] [Module R N]
+
+/-- Two linearly equivalent modules have the same corank. -/
+theorem corank_map_eq (e : M ≃ₗ[R] N) (p : Submodule R M) :
+    (p.map (e : M →ₗ[R] N)).corank = p.corank := by
+  exact (Submodule.Quotient.equiv p (p.map (e : M →ₗ[R] N)) e rfl).rank_eq.symm
+
+end LinearEquiv
+
+namespace Submodule
+
+-- # Rank Nullity
+
+variable {M : Type v}
+variable [Ring R] [AddCommGroup M] [Module R M]
+variable [HasRankNullity.{v} R]
+
+theorem corank_add_rank (p : Submodule R M) :
+    p.corank + Module.rank R p = Module.rank R M := by
+  simpa [corank] using rank_quotient_add_rank p
+
+/-- The submodule `q.map p.mkQ` of `M ⧸ p` represents the relative quotient
+`q / p`. This theorem says that the corank drops, from `p` to `q`, by the
+rank of `q / p`.
+-/
+theorem corank_add_rank_quotient {p q : Submodule R M} (h : p ≤ q) :
+    q.corank + Module.rank R (q.map p.mkQ) = p.corank := by
+  change Module.rank R (M ⧸ q) + Module.rank R (q.map p.mkQ) = Module.rank R (M ⧸ p)
+  rw [← (p.quotientQuotientEquivQuotient q h).rank_eq]
+  exact rank_quotient_add_rank (q.map p.mkQ)
 
 end Submodule
