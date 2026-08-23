470a66f6b11632485b67b2f869ce1d59b6d77b7a
Martin Winter <martin.winter.math@gmail.com>
2026-08-23 13:54:43 +0200

    mk_all

--- Polyhedral.lean
+++ Polyhedral.lean
@@ -51,6 +51,7 @@ import Polyhedral.Mathlib.Geometry.Convex.ConvexSpace.Polytope.Pointwise
 import Polyhedral.Mathlib.Geometry.Convex.ConvexSpace.Set.Basic
 import Polyhedral.Mathlib.Geometry.Convex.ConvexSpace.Set.Face.Basic
 import Polyhedral.Mathlib.Geometry.Convex.ConvexSpace.Set.Face.Homogenization
+import Polyhedral.Mathlib.Geometry.Convex.ConvexSpace.Set.Face.Lattice
 import Polyhedral.Mathlib.Geometry.Convex.ConvexSpace.Set.Homogenization
 import Polyhedral.Mathlib.Geometry.Convex.ConvexSpace.Set.Hull
 import Polyhedral.Mathlib.Geometry.Convex.ConvexSpace.Set.Lattice
